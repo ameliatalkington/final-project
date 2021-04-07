4ba42b285406fb18778d02f2d820ded734fb6a93
ameliatalkington <amrtalk@stanford.edu>
2021-04-07 02:55:36 +0000

    Added buttons and search input

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+const styles = {
+
+};
+
 export default function Home(props) {
   return (
     <>
+      <div className='container'>
+        <div className='buttons'>
+          <button>search</button>
+          <button>filter</button>
+        </div>
+        <form>
+          <input type="search" placeholder='city, state, country, zip'/>
+        </form>
+      </div>
     </>
   );
 }
